2f51a0c3de3e417674add43023cd63395052be22
C8linR <45794604+c8linr@users.noreply.github.com>
2021-04-24 11:59:26 -0400

    move include files to folder

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <small>Copyright belongs to Caitlin Ross, &copy;CAJR Fibre 2021. 
-      Flag icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a>
-      from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>. 
-      All other icons from <a href="https://fontawesome.com/" title="Font Awesome">FontAwesome</a>.
-    </small>
-    <script src="include/scripts.js"></script>
-</footer>
